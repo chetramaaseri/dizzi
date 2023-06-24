38875fee35cd979f44a23098e330b7f62f88879f
chetramaaseri <90443950+chetramaaseri@users.noreply.github.com>
2023-06-24 14:39:01 +0530

    otherPages

--- src/components/capabilities/Capabilities.js
+++ src/components/capabilities/Capabilities.js
@@ -2,14 +2,7 @@ import React from 'react'
 import TextContent from "../layout/textContent/TextContent"
 import Grid4 from '../services/Grid4/Grid4'
 import Vector from '../layout/vector/Vector'
-function Capabilities() {
-  const capabilityText = {
-    highlight : "Have a Look We are Amazing 😉",
-    heading: "Creative Portfolio is a Beast <br>Boom Boom💥 Yo ✌️",
-    text : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid consectetur velit voluptatibus magni. Magnam debitis dignissimos eos explicabo sequi modi incidunt repudiandae, iste facilis vel! Non laboriosam quisquam aliquam nesciunt sed alias consequatur tempora, repudiandae autem a accusantium veritatis dolore fugit voluptatum odit est nulla rem laudantium neque eveniet laborum!",
-    btnText : "Explore Us",
-    alignment: "yCenter"
-  }
+function Capabilities({data}) {
   const vectorCapabilitiesRight = {
     src : "https://dizzi.netlify.app/images/green-left.svg",
     height: "660px",
@@ -24,10 +17,10 @@ function Capabilities() {
       <Vector vectorData={vectorCapabilitiesRight}/>
       <div className="container">
         <div className='row mt-5'>
-          <TextContent data={capabilityText}/>
+          <TextContent data={data.textContent}/>
           <div className="col-12 mt-3">
-            <Grid4 orientation={{
-              main : "d-flex flex-wrap flex-md-nowrap flex-row allCenter"
+            <Grid4 data={data.grid} orientation={{
+              main : "d-flex flex-wrap flex-md-nowrap flex-row xCenter"
             }}/>
           </div>
         </div>
--- src/components/hero/vertical/HeroVertical.js
+++ src/components/hero/vertical/HeroVertical.js
@@ -4,7 +4,7 @@ import Breadcrum from '../../layout/Breadcrum'
 import Grid4 from '../../services/Grid4/Grid4'
 import Vector from '../../layout/vector/Vector'
 
-function HeroVertical() {
+function HeroVertical({data}) {
   const vectorHeroLeftTop = {
     src : "https://dizzi.netlify.app/images/yellow-left.svg",
     height: "360px",
@@ -29,14 +29,14 @@ function HeroVertical() {
       <Vector vectorData={vectorHeroRight}/>
       <div className="container">
         <div className='HeroVertical row mt-5'>
-            <Breadcrum page={"What WE Do?"}/>
-            <span className='highlight mt-5'>WHAT WE DO</span>
-            <HeroText orientation={{
+            <Breadcrum page={data.breadcrum}/>
+            <span className='highlight mt-5'>{data.highlight}</span>
+            <HeroText data={data.heroText} orientation={{
                 main : "col-sm-6 mt-3",
                 content : "w-75 mt-3"
             }} />
             <div className="col-sm-6">
-                <Grid4 orientation={{
+                <Grid4 data={data.grid} orientation={{
                   main : "d-flex flex-column text-center text-sm-start gap-5 d-sm-grid"
                 }}/>
             </div>
